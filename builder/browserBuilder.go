a216f97be9239187f160708deb330312d4ccec5f
Mansoor <mansoor@mansoorsayed.com>
2022-08-07 05:32:48 -0400

    Removed large amounts of duplicated code in builder

--- builder/browserBuilder.go
+++ builder/browserBuilder.go
@@ -9,14 +9,10 @@ import (
 	esbuild "github.com/evanw/esbuild/pkg/api"
 	"github.com/mansoor-s/aviator/js"
 	"github.com/mansoor-s/aviator/utils"
-	"os"
-	"path"
 	"path/filepath"
 	"strings"
 	"text/template"
 )
-
-const aviatorRuntimeTargetID = "__aviator_root"
 
 type BrowserImports struct {
 	JS  []string
@@ -50,7 +46,7 @@ func NewBrowserBuilder(
 
 //BuildDev creates assets for embedding into the rendered view
 // It persists the assets into the output directory
-func (b *BrowserBuilder) BuildDev(ctx context.Context) error {
+func (b *BrowserBuilder) BuildDev(_ context.Context) error {
 	views := b.viewManager.AllViews()
 	viewsByEntryPoint := make(map[string]*View, len(views))
 	viewsByOutputName := make(map[string]*View, len(views))
@@ -93,9 +89,9 @@ func (b *BrowserBuilder) BuildDev(ctx context.Context) error {
 		Plugins: append(
 			[]esbuild.Plugin{
 				b.browserRuntimePlugin(viewsByEntryPoint),
-				b.wrappedComponentsPlugin(),
-				b.svelteComponentsPlugin(),
-				b.npmJsPathPlugin(),
+				wrappedComponentsPlugin(b.workingDir, b.viewManager, b.browserCompile),
+				svelteComponentsPlugin(b.workingDir, b.browserCompile),
+				npmJsPathPlugin(b.workingDir),
 			},
 		),
 		Write: true,
@@ -110,10 +106,10 @@ func (b *BrowserBuilder) BuildDev(ctx context.Context) error {
 	}
 
 	//delete all old generated files
-	//err := utils.RemoveDirContents(b.outputDir)
-	//if err != nil {
-	//	return err
-	//}
+	err := utils.RemoveDirContents(b.outputDir)
+	if err != nil {
+		return err
+	}
 
 	for _, file := range result.OutputFiles {
 		fileName := filepath.Base(file.Path)
@@ -180,184 +176,6 @@ func (b *BrowserBuilder) browserRuntimePlugin(viewsByOutputName map[string]*View
 		},
 	}
 
-}
-
-//wrappedComponentsPlugin creates a new virtual svelte component that
-// composes all Svelte Components with all the layouts that apply to them
-// i.e:  <RootLayout><FooLayout><MyComponent></MyComponent></FooLayout></RootLayout>
-func (b *BrowserBuilder) wrappedComponentsPlugin() esbuild.Plugin {
-	//index views by their WrappedUniqueName for easier lookup in plugin
-	viewsByWrappedName := make(map[string]*View)
-	for _, view := range b.viewManager.AllViews() {
-		viewsByWrappedName[view.WrappedUniqueName] = view
-	}
-
-	return esbuild.Plugin{
-		Name: "wrappedComponents",
-		Setup: func(epb esbuild.PluginBuild) {
-			epb.OnResolve(
-				//__AviatorWrapped{UniqueName}.svelte
-				esbuild.OnResolveOptions{Filter: `^__AviatorWrapped_.*\.svelte$`},
-				func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
-					result.Namespace = "wrappedComponents"
-					result.Path = args.Path
-					return result, nil
-				},
-			)
-
-			epb.OnLoad(
-				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "wrappedComponents"},
-				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
-
-					//get the wrapped unique name by removing the extension
-					wrappedName := args.Path
-					fileExt := filepath.Ext(args.Path)
-					if len(fileExt) > 0 {
-						wrappedName = wrappedName[:len(wrappedName)-len(fileExt)]
-					}
-
-					view, ok := viewsByWrappedName[wrappedName]
-					if !ok {
-						return result, fmt.Errorf(
-							"unable to find wrapped component named: %s", wrappedName,
-						)
-					}
-
-					rawVirtualCode := createLayoutWrappedView(view)
-
-					compiledCode, err := b.browserCompile(args.Path, []byte(rawVirtualCode))
-					if err != nil {
-						return result, err
-					}
-
-					contents := compiledCode.JS
-					result.ResolveDir = b.workingDir
-					result.Contents = &contents
-					result.Loader = esbuild.LoaderJSX
-					return result, nil
-				},
-			)
-		},
-	}
-}
-
-//svelteComponentsPlugin handles .svelte files both inside the project and node_modules
-func (b *BrowserBuilder) svelteComponentsPlugin() esbuild.Plugin {
-	return esbuild.Plugin{
-		Name: "browserSvelte",
-		Setup: func(epb esbuild.PluginBuild) {
-			epb.OnResolve(
-				esbuild.OnResolveOptions{Filter: `^.*\.svelte$`},
-				func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
-					callerPath := filepath.Dir(args.Importer)
-					var absPath string
-					if callerPath == "." {
-						absPath = path.Join(args.ResolveDir, args.Path)
-					} else {
-						absPath, err = filepath.Abs(path.Join(callerPath, args.Path))
-						if err != nil {
-							return result, err
-						}
-					}
-
-					result.Path = absPath
-					result.Namespace = "svelte"
-					return result, nil
-				})
-
-			epb.OnLoad(
-				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "svelte"},
-				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
-
-					rawCode, err := os.ReadFile(args.Path)
-					if err != nil {
-						return result, err
-					}
-
-					newPath := utils.PathPascalCase(filepath.Base(args.Path))
-
-					compiledCode, err := b.browserCompile(newPath, rawCode)
-					if err != nil {
-						return result, err
-					}
-
-					contents := compiledCode.JS
-					result.ResolveDir = b.workingDir
-					result.Contents = &contents
-					result.Loader = esbuild.LoaderJSX
-					return result, nil
-				},
-			)
-		},
-	}
-}
-
-func (b *BrowserBuilder) npmJsPathPlugin() esbuild.Plugin {
-	return esbuild.Plugin{
-		Name: "js_path",
-		Setup: func(epb esbuild.PluginBuild) {
-			//handles imports that are JS files, but for some reason the import path doesn't
-			//include the .js suffix
-			epb.OnResolve(
-				esbuild.OnResolveOptions{Filter: `\.+\/(.*\/)?[a-zA-Z0-9]+$`},
-				func(args esbuild.OnResolveArgs) (esbuild.OnResolveResult, error) {
-					var result esbuild.OnResolveResult
-
-					importedFilePath := args.Path
-
-					fileExt := filepath.Ext(args.Path)
-					if len(fileExt) == 0 {
-						importedFilePath += ".js"
-					}
-
-					callerPath := filepath.Dir(args.Importer)
-					absPath, err := filepath.Abs(path.Join(callerPath, importedFilePath))
-					if err != nil {
-						return result, err
-					}
-
-					result.Path = absPath
-					result.Namespace = "js_path"
-
-					return result, nil
-				},
-			)
-
-			epb.OnResolve(
-				esbuild.OnResolveOptions{Filter: `^.*\.js$`},
-				func(args esbuild.OnResolveArgs) (esbuild.OnResolveResult, error) {
-					var result esbuild.OnResolveResult
-
-					callerPath := filepath.Dir(args.Importer)
-					absPath, err := filepath.Abs(path.Join(callerPath, args.Path))
-					if err != nil {
-						return result, err
-					}
-
-					result.Path = absPath
-					result.Namespace = "js_path"
-
-					return result, nil
-				},
-			)
-
-			epb.OnLoad(
-				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "js_path"},
-				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
-					rawCode, err := os.ReadFile(args.Path)
-					if err != nil {
-						return result, err
-					}
-
-					contents := string(rawCode)
-					result.ResolveDir = b.workingDir
-					result.Contents = &contents
-					result.Loader = esbuild.LoaderJS
-					return result, nil
-				},
-			)
-		},
-	}
 }
 
 func (b *BrowserBuilder) browserCompile(path string, code []byte) (*SvelteBuildOutput, error) {
---
+++ builder/commonPlugins.go
@@ -0,0 +1,233 @@
+package builder
+
+import (
+	"fmt"
+	esbuild "github.com/evanw/esbuild/pkg/api"
+	"github.com/mansoor-s/aviator/utils"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+const wrappedScriptFmt = "<script>\n%s\n</script>\n"
+const wrappedImportStatementFmt = "import %s from \"%s\""
+
+func createLayoutWrappedView(view *View) string {
+	layouts := view.ApplicableLayoutViews
+
+	var importStatements []string
+	var startTags []string
+	var endTags []string
+
+	for _, layout := range layouts {
+		importStatement := fmt.Sprintf(wrappedImportStatementFmt, layout.UniqueName, layout.RelPath)
+		importStatements = append(importStatements, importStatement)
+
+		startStr := `<` + layout.UniqueName + `>`
+		startTags = append(startTags, startStr)
+
+		endStr := `</` + layout.UniqueName + `>`
+		endTags = append([]string{endStr}, endTags...)
+	}
+	importStatement := fmt.Sprintf(wrappedImportStatementFmt, view.UniqueName, view.RelPath)
+	importStatements = append(importStatements, importStatement)
+
+	componentStr := `<svelte:component this={` + view.UniqueName + `}  {...($$props || {})}/>`
+
+	wrappedComponentStr := strings.Join(startTags, "") +
+		componentStr +
+		strings.Join(endTags, "")
+
+	allImportStatements := strings.Join(importStatements, "\n")
+	wrappedSvelteComponent := fmt.Sprintf(wrappedScriptFmt, allImportStatements)
+
+	return wrappedSvelteComponent + wrappedComponentStr
+}
+
+func npmJsPathPlugin(workingDir string) esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "js_path",
+		Setup: func(epb esbuild.PluginBuild) {
+			//handles imports that are JS files, but for some reason the import path doesn't
+			//include the .js suffix
+			epb.OnResolve(
+				esbuild.OnResolveOptions{Filter: `\.+\/(.*\/)?[a-zA-Z0-9]+$`},
+				func(args esbuild.OnResolveArgs) (esbuild.OnResolveResult, error) {
+					var result esbuild.OnResolveResult
+
+					importedFilePath := args.Path
+
+					fileExt := filepath.Ext(args.Path)
+					if len(fileExt) == 0 {
+						importedFilePath += ".js"
+					}
+
+					callerPath := filepath.Dir(args.Importer)
+					absPath, err := filepath.Abs(path.Join(callerPath, importedFilePath))
+					if err != nil {
+						return result, err
+					}
+
+					result.Path = absPath
+					result.Namespace = "js_path"
+
+					return result, nil
+				},
+			)
+
+			epb.OnResolve(
+				esbuild.OnResolveOptions{Filter: `^.*\.js$`},
+				func(args esbuild.OnResolveArgs) (esbuild.OnResolveResult, error) {
+					var result esbuild.OnResolveResult
+
+					callerPath := filepath.Dir(args.Importer)
+					absPath, err := filepath.Abs(path.Join(callerPath, args.Path))
+					if err != nil {
+						return result, err
+					}
+
+					result.Path = absPath
+					result.Namespace = "js_path"
+
+					return result, nil
+				},
+			)
+
+			epb.OnLoad(
+				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "js_path"},
+				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+					rawCode, err := os.ReadFile(args.Path)
+					if err != nil {
+						return result, err
+					}
+
+					contents := string(rawCode)
+					result.ResolveDir = workingDir
+					result.Contents = &contents
+					result.Loader = esbuild.LoaderJS
+					return result, nil
+				},
+			)
+		},
+	}
+}
+
+type SvelteCompilerFunc func(string, []byte) (*SvelteBuildOutput, error)
+
+//wrappedComponentsPlugin creates a new virtual svelte component that
+// composes all Svelte Components with all the layouts that apply to them
+// i.e:  <RootLayout><FooLayout><MyComponent></MyComponent></FooLayout></RootLayout>
+func wrappedComponentsPlugin(
+	workingDir string,
+	viewManager *ViewManager,
+	compilerFunc SvelteCompilerFunc,
+) esbuild.Plugin {
+	//index views by their WrappedUniqueName for easier lookup in plugin
+	viewsByWrappedName := make(map[string]*View)
+	for _, view := range viewManager.AllViews() {
+		viewsByWrappedName[view.WrappedUniqueName] = view
+	}
+
+	return esbuild.Plugin{
+		Name: "wrappedComponents",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(
+				//__AviatorWrapped{UniqueName}.svelte
+				esbuild.OnResolveOptions{Filter: `^__AviatorWrapped_.*\.svelte$`},
+				func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+					result.Namespace = "wrappedComponents"
+					result.Path = args.Path
+					return result, nil
+				},
+			)
+
+			epb.OnLoad(
+				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "wrappedComponents"},
+				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+
+					//get the wrapped unique name by removing the extension
+					wrappedName := args.Path
+					fileExt := filepath.Ext(args.Path)
+					if len(fileExt) > 0 {
+						wrappedName = wrappedName[:len(wrappedName)-len(fileExt)]
+					}
+
+					view, ok := viewsByWrappedName[wrappedName]
+					if !ok {
+						return result, fmt.Errorf(
+							"unable to find wrapped component named: %s", wrappedName,
+						)
+					}
+
+					rawVirtualCode := createLayoutWrappedView(view)
+
+					compiledCode, err := compilerFunc(args.Path, []byte(rawVirtualCode))
+					if err != nil {
+						return result, err
+					}
+
+					contents := compiledCode.JS
+					result.ResolveDir = workingDir
+					result.Contents = &contents
+					result.Loader = esbuild.LoaderJSX
+					return result, nil
+				},
+			)
+		},
+	}
+}
+
+//svelteComponentsPlugin handles .svelte files both inside the project and node_modules
+func svelteComponentsPlugin(
+	workingDir string,
+	compilerFunc SvelteCompilerFunc,
+) esbuild.Plugin {
+	return esbuild.Plugin{
+		Name: "svelte",
+		Setup: func(epb esbuild.PluginBuild) {
+			epb.OnResolve(
+				esbuild.OnResolveOptions{Filter: `^.*\.svelte$`},
+				func(args esbuild.OnResolveArgs) (result esbuild.OnResolveResult, err error) {
+					callerPath := filepath.Dir(args.Importer)
+					var absPath string
+					if callerPath == "." {
+						absPath = path.Join(args.ResolveDir, args.Path)
+					} else {
+						absPath, err = filepath.Abs(path.Join(callerPath, args.Path))
+						if err != nil {
+							return result, err
+						}
+					}
+
+					result.Path = absPath
+					result.Namespace = "svelte"
+					return result, nil
+				})
+
+			epb.OnLoad(
+				esbuild.OnLoadOptions{Filter: `.*`, Namespace: "svelte"},
+				func(args esbuild.OnLoadArgs) (result esbuild.OnLoadResult, err error) {
+
+					rawCode, err := os.ReadFile(args.Path)
+					if err != nil {
+						return result, err
+					}
+
+					newPath := utils.PathPascalCase(filepath.Base(args.Path))
+
+					compiledCode, err := compilerFunc(newPath, rawCode)
+					if err != nil {
+						return result, err
+					}
+
+					contents := compiledCode.JS
+					result.ResolveDir = workingDir
+					result.Contents = &contents
+					result.Loader = esbuild.LoaderJSX
+					return result, nil
+				},
+			)
+		},
+	}
+}
